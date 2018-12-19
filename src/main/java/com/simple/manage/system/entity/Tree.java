7d8bfbed5c5656f10168a0c6290840fb800c977a
chen <chendreaming@126.com>
2018-12-19 19:58:14 +0800

    celebration day

--- src/main/java/com/simple/manage/system/entity/Tree.java
+++ src/main/java/com/simple/manage/system/entity/Tree.java
@@ -9,7 +9,6 @@ import java.util.List;
  * Author chen
  * CreateTime 2018-12-18 11:44
  **/
-@Data
 public class Tree {
     private int key;
 
@@ -18,4 +17,36 @@ public class Tree {
     private boolean isLeaf;
 
     private List<Tree> children;
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean getIsLeaf() {
+        return isLeaf;
+    }
+
+    public void setIsLeaf(boolean leaf) {
+        isLeaf = leaf;
+    }
+
+    public List<Tree> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Tree> children) {
+        this.children = children;
+    }
 }
