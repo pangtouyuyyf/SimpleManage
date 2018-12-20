e0209fd3ea1d4b87eea7c4647a79a0de8034f7ce
chen <chendreaming@126.com>
2018-12-20 15:31:39 +0800

    celebration day

--- src/main/java/com/simple/manage/system/config/WebSocketConfig.java
+++ src/main/java/com/simple/manage/system/config/WebSocketConfig.java
@@ -1,5 +1,7 @@
 package com.simple.manage.system.config;
 
+import com.simple.manage.system.interceptor.WSInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -17,13 +19,15 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableScheduling
 //通过EnableWebSocketMessageBroker 开启使用STOMP协议来传输基于代理(message broker)的消息,此时浏览器支持使用@MessageMapping 就像支持@RequestMapping一样。
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Autowired
+    private WSInterceptor wsInterceptor;
     /**
      * endPoint 注册STOMP协议的节点(endpoint),并映射指定的url
      *
      * @param registry
      */
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/socket").setAllowedOrigins("*");
+        registry.addEndpoint("/socket").addInterceptors(wsInterceptor).setAllowedOrigins("*");
     }
 
     /**
