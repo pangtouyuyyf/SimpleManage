997c9b99946bfd18c0c9bb26d1893e751cc4b6f7
chen <chendreaming@126.com>
2019-01-02 10:35:30 +0800

    celebration day

---
+++ src/main/java/com/simple/manage/system/service/impl/CorporationServiceImpl.java
@@ -0,0 +1,70 @@
+package com.simple.manage.system.service.impl;
+
+import com.simple.manage.system.dao.CorporationDao;
+import com.simple.manage.system.service.CorporationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Description 公司服务接口实现
+ * Author chen
+ * CreateTime 2019-01-02 10:31
+ **/
+@Service
+public class CorporationServiceImpl implements CorporationService {
+    @Autowired
+    private CorporationDao corporationDao;
+
+    /**
+     * 添加或更新公司信息
+     *
+     * @param corp
+     * @return
+     */
+    public int addOrUpdCorp(Map<String, Object> corp) {
+        return this.corporationDao.addOrUpdCorp(corp);
+    }
+
+    /**
+     * 查询公司信息
+     *
+     * @param corpId
+     * @return
+     */
+    public Map<String, Object> queryCorp(int corpId) {
+        return this.corporationDao.queryCorp(corpId);
+    }
+
+    /**
+     * 查询公司信息列表
+     *
+     * @param params
+     * @return
+     */
+    public List<Map<String, Object>> queryCorpList(Map<String, Object> params) {
+        return this.corporationDao.queryCorpList(params);
+    }
+
+    /**
+     * 逻辑删除公司信息
+     *
+     * @param params
+     * @return
+     */
+    public int delCorp(Map<String, Object> params) {
+        return this.corporationDao.delCorp(params);
+    }
+
+    /**
+     * 删除公司信息
+     *
+     * @param corpId
+     * @return
+     */
+    public int delCorpForReal(int corpId) {
+        return this.corporationDao.delCorpForReal(corpId);
+    }
+}
